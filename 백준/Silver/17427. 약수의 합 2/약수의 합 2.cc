0ece60fad247d29de47c9a821e7ea2fe54a2fed9
한동민 <99469344+mindeulle7@users.noreply.github.com>
2022-07-10 07:44:47 +0900

    [Silver II] Title: 약수의 합 2, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/17427. 약수의 합 2/약수의 합 2.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n;
+    long long sum = 0;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        sum += i * (n / i);
+    }
+    cout << sum;
+}
