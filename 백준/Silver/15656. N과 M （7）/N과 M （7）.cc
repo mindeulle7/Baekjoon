9f1c6115cbfaebc4f533119b5e0bca05afe8e6af
한동민 <99469344+mindeulle7@users.noreply.github.com>
2022-07-22 04:19:35 +0900

    [Silver III] Title: N과 M (7), Time: 772 ms, Memory: 65284 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<vector<int>> res;
+void dfs(int n, vector<int> v, vector<int> &temp);
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n , m;
+    cin >> n >> m;
+    vector<int> v;
+    vector<int> temp(m);
+    for(int i=0; i<n; i++)
+    {
+        int t;
+        cin >> t;
+        v.push_back(t);
+    }
+    dfs(0,v, temp);
+    sort(res.begin(),res.end());
+    res.erase(unique(res.begin(),res.end()),res.end());
+    for(int i=0; i<res.size();i++)
+    {
+        for(int j=0; j<res[0].size(); j++)
+        {
+            cout << res[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
+
+void dfs(int n, vector<int> v, vector<int> &temp)
+{
+    if(n == temp.size())
+    {
+        res.push_back(temp);
+        return;
+    }
+    else
+    {
+        for(int i=0; i<v.size(); i++)
+        {
+            temp[n] = v[i];
+            dfs(n+1,v,temp);
+        }
+    }
+}
