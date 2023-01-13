8cd199aac8257b5ccad4444d066391127765cc3f
한동민 <99469344+mindeulle7@users.noreply.github.com>
2023-01-13 13:28:00 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string t, string p) {
+    int answer = 0;
+    for(int i=0; i<t.size()-p.size()+1; i++){
+        string temp = "";
+        for(int j=i; j<i+p.size(); j++){
+            temp += t[j];
+        }
+        if(temp <= p)   answer++;
+    }
+    return answer;
+}
